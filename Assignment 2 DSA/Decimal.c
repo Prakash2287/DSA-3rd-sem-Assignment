06c41b35108a395dda1ad1f8e54a0ec4e70547a0
Prakash2287 <88587342+Prakash2287@users.noreply.github.com>
2021-10-18 20:51:38 +0530

    Add files via upload

---
+++ Assignment 2 DSA/Decimal.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("Addition in arabic system:-");
+    printf("%d",(a+b));
+    printf("\n");
+    printf("Multiplication in arabic system:-");
+    printf("%d",(a*b));
+	// your code goes here
+	return 0;
+}
+
